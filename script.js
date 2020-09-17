9f27840095a5b1c3768383daf7a8fc5d52f0522e
FlameFlashy <dim0k92sokol@gmail.com>
2020-09-17 16:48:55 +0300

    end project

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
         $('.header-burger,.header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    $('.header_menu').click(function(){
+        $('.header-burger,.header_menu').removeClass('active');
+        $('body').removeClass('lock');
+ });
 })
